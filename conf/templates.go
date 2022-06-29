effc910d77201ccf2934c9029be1916c4868a926
princebillygk <princebillygk@gmail.com>
2022-06-30 02:19:11 +0600

    updated path for templates

--- conf/templates.go
+++ conf/templates.go
@@ -2,10 +2,11 @@ package conf
 
 import (
 	"html/template"
+	"path/filepath"
 )
 
-var templatePath = "../../frontend/templates/"
+var templatePath, _ = filepath.Abs("../../frontend/templates")
 
 var Templates = map[string]*template.Template{
-	"homepage": template.Must(template.ParseFiles(templatePath + "index.tmpl")),
+	"homepage": template.Must(template.ParseFiles(templatePath + "/index.tmpl")),
 }
